c2aca174881123a67a8f5aa939a5f79ed07ac14b
Aeyan-Raza-pk <www.aeyanraza65@gmail.com>
2025-06-22 23:08:31 +0500

    add files

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://job-portal-webapp-frontend.vercel.app"], 
   credentials: true,
 };
 app.use(cors(corsOptions));
