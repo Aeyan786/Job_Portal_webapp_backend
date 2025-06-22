4056525d37a32fb4eb31525b996fddb26d629d30
Aeyan-Raza-pk <www.aeyanraza65@gmail.com>
2025-06-22 23:42:29 +0500

    token update

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -97,7 +97,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: "None",
       })
       .json({
         message: `Welcome Back ${loggedInUser.fullname}`,
